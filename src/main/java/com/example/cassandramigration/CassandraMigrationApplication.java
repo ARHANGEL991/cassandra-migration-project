d2a2c74cb22719898606faa82ec6ff37eb11dd02
Роман Кухтини <romakuhta991@gmail.com>
2018-03-26 10:44:32 +0300

    Initial commit

---
+++ src/main/java/com/example/cassandramigration/CassandraMigrationApplication.java
@@ -0,0 +1,12 @@
+package com.example.cassandramigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraMigrationApplication.class, args);
+	}
+}
